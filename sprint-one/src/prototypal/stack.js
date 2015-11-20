d7b84b478bd86ef34e1174d339794763978b879d
GMeyr <gabrielmeyr@gmail.com>
2015-11-20 13:26:21 -0500

    only stack for pseudoclassical to do now

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,48 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods); //.prototype?
+  var timesCalledPush = 0, timesCalledPop = 0, toPop = 0;
+  obj.timesCalledPush = timesCalledPush;
+  obj.timesCalledPop = timesCalledPop;
+  obj.toPop = toPop;
+
+  return obj;
+  
 };
+
 
 var stackMethods = {};
 
+  stackMethods.push = function(value){
+    this[this.timesCalledPush] = value;
+    if(this.timesCalledPush === 0){
+      this.toPop = 0;
+      this.timesCalledPush++;
+    } else {
+      this.toPop++;
+      this.timesCalledPush++;
+    }
+    
 
+  };
+
+  stackMethods.pop = function(){
+
+    var temporary = this[this.toPop];
+    delete this[this.toPop];
+    this.timesCalledPop++;
+    this.toPop--;
+    if(this.toPop < 0){
+      this.toPop = 0;
+    }
+    return temporary;
+
+  };
+
+  stackMethods.size = function(){
+    if (this.timesCalledPush - this.timesCalledPop < 0){
+      return 0;
+    }
+    return this.timesCalledPush - this.timesCalledPop;
+  };
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.timesEnqueueCalled = 0;
+  this.timesDequeueCalled = 0;
+  this.toDequeue = 0;
 };
 
+Queue.prototype.enqueue = function (value) {
+  this[this.timesEnqueueCalled] = value;
+  this.timesEnqueueCalled++;
+};
 
+Queue.prototype.dequeue = function () {
+  var temp = this[this.toDequeue];
+  delete this[this.toDequeue];
+  this.timesDequeueCalled++;
+  this.toDequeue++;
+  return temp;
+};
+
+Queue.prototype.size = function () {
+  if(this.timesEnqueueCalled - this.timesDequeueCalled < 0){
+  	return 0;
+  } else {
+  	return this.timesEnqueueCalled - this.timesDequeueCalled;
+  }
+};
