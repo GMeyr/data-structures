38eb1b0b3bc7e9a23ff829182f1e3736bb0a206c
GMeyr <gabrielmeyr@gmail.com>
2015-11-20 11:54:58 -0500

    solved prototype for queue

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,50 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.timesCalledPop = 0;
+  someInstance.timesCalledPush = 0;
+  someInstance.toPop = 0; 
+
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+
+  return someInstance;
+
 };
 
 var stackMethods = {};
 
+  stackMethods.push = function(value){
+    this[this.timesCalledPush] = value;
+    if(this.timesCalledPush === 0){
+      this.toPop = 0;
+      this.timesCalledPush++;
+    } else {
+      this.toPop++;
+      this.timesCalledPush++;
+    }
+    
 
+  };
+
+  stackMethods.pop = function(){
+
+    var temporary = this[this.toPop];
+    delete this[this.toPop];
+    this.timesCalledPop++;
+    this.toPop--;
+    if(this.toPop < 0){
+      this.toPop = 0;
+    }
+    return temporary;
+
+  };
+
+  stackMethods.size = function(){
+    if (this.timesCalledPush - this.timesCalledPop < 0){
+      return 0;
+    }
+    return this.timesCalledPush - this.timesCalledPop;
+  };
--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,41 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods); //.prototype?
+  var timesCalledEnqueue = 0, timesCalledDequeue = 0, toDequeue = 0;
+  obj.timesCalledEnqueue = timesCalledEnqueue;
+  obj.timesCalledDequeue = timesCalledDequeue;
+  obj.toDequeue = toDequeue;
+
+  return obj;
 };
 
 var queueMethods = {};
 
 
+
+  queueMethods.enqueue = function(value){
+    this[this.timesCalledEnqueue] = value;
+    this.timesCalledEnqueue += 1;
+  };
+
+  queueMethods.dequeue = function(){
+
+    var temporary = this[this.toDequeue];
+
+    delete this[this.toDequeue];
+    this.timesCalledDequeue +=1;
+    this.toDequeue+=1;  
+    return temporary;
+
+
+  };
+
+  queueMethods.size = function(){
+      if (this.timesCalledEnqueue-this.timesCalledDequeue < 0){
+        return 0;
+      } else{
+      return this.timesCalledEnqueue-this.timesCalledDequeue;
+      }
+
+  };
