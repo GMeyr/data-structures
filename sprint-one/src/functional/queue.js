a416c8f9d38839e66d863d428798d38bb0b3f782
Taylor Richards <taylormaliarichards@192.168.1.10>
2015-11-20 10:17:48 -0500

    sean got it to pass last func queue test

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,41 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var timesCalledEnqueue = 0;
+  var timesCalledDequeue = 0;
+  var toDequeue = 0;
 
   // Implement the methods below
+  // storage Object {};
+  // storage["1"] = "hi";
+  // storage.length === undefined
+  // enqueue last value and dequeue first value
 
   someInstance.enqueue = function(value){
+    storage[timesCalledEnqueue] = value;
+    timesCalledEnqueue += 1;
   };
 
   someInstance.dequeue = function(){
+
+    var temporary = {};
+    temporary["answer"] = storage[toDequeue];
+
+    delete storage[toDequeue];
+    timesCalledDequeue +=1;
+    toDequeue+=1;  
+    return temporary["answer"];
+
+
   };
 
   someInstance.size = function(){
+      if (timesCalledEnqueue-timesCalledDequeue < 0){
+        return 0;
+      } else{
+      return timesCalledEnqueue-timesCalledDequeue;
+      }
+
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,34 @@
 var Stack = function(){
   var someInstance = {};
+  var counter = 0;
+  var topOfStack = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+  
+    storage[counter] = value;
+    counter++;
+
   };
 
   someInstance.pop = function(){
+
+    var temporary = {};
+    temporary["answer"] = storage[counter];
+    counter--;
+    delete storage[counter];
+    return temporary["answer"];
+
   };
 
   someInstance.size = function(){
+    if (counter-topOfStack < 0){
+      return 0;
+    }
+    return counter;
   };
 
   return someInstance;
