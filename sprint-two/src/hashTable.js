8d387de5c9ddb6745ce340b2a98a2f789a064d51
GMeyr <gabrielmeyr@gmail.com>
2015-11-30 11:50:38 -0500

    messing with hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,33 +1,20 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.pair = [];
 };
+
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+    if(k === 'val1'){
+      debugger;
+    }
+    var bucket = this._storage.get(i);
+    this.pair.push([k,v]);
 
-	
-  	var bucket = this._storage.get(i);
-  	var pair = [k,v];
-
-    if(!bucket){
- 	  this._storage.set(i,pair); 	
-
-    } else if (this._storage.get(i)[0] === k){
-      this._storage.set(i, pair)
-
-    } else {
-      for (var j = 0; j < bucket.length; j++){
-  	    if(bucket[j][0] === k){
-  	      bucket[j][1] = v;
-  	    } else{
-  		bucket.push(pair);
-  	    }
-      }
-  	this._storage.set(i,bucket);
-  }
-
-};
+    this._storage.set(i, this.pair);  
+  };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
