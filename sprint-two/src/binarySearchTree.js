b02b6bd364cb1d4a7c35504b9e8368978e9b6deb
GMeyr <gabrielmeyr@gmail.com>
2015-11-30 16:31:58 -0500

    solved binary search tree as pair

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -15,92 +15,68 @@ var binarySearchTree = {};
 //compare new value to current value
 
 binarySearchTree.insert = function(value){
-  var newNode = BinarySearchTree(value);
 
-  var placeNode = function(older, newer){
-    if(newer.value < older.value){
-  	  if(older.left) {
-  	    placeNode(older.left, newer);
-      } else {
-      	older.left = newer;
-      }
+var newNode = BinarySearchTree(value);
+
+var placeNode = function(older,newer){
+  if (newer.value < older.value){
+    if (older.left){
+      placeNode(older.left,newer);
     } else {
-      if(older.right){
-        placeNode(older.right, newer);
-      } else {
-      	older.right = newer;
-      }
+      older.left = newer;
     }
+  } else {
+    if(older.right){
+      placeNode(older.right,newer);
+    } else{
+        older.right = newer;
+      }
   }
+}
 
-  placeNode(this, newNode);
+placeNode(this,newNode);
 
-	// var newNode = BinarySearchTree(value);
-	//   if(this.left.value >= newNode.value){
-	// 	this.right = this.left;
-	// 	this.left = newNode;
-	//   } else {
-	//   	this.right = newNode;
-	//   }
-	// } else if (this.count === 1){
-	// 	this.left = newNode;
-	// }
 
-	
+  
 };
 
 binarySearchTree.contains = function(value){
-  if(this.value === value){
-  	return true;
-  } else {
-  	if(this.value > value){
-  	  if(this.left){
-  	  	return this.left.contains(value);
-  	  } else {
-  	  	return false;
-  	  }
-  	} else {
-  	  if(this.right){
-  	  	return this.right.contains(value);
-  	  } else {
-        return false;
-  	  }
-  	}
+
+if(this.value === value){
+  return true;
+} else if (this.value > value) {
+  if(this.left){
+    return this.left.contains(value);
   }
-  return false;
+}
+
+  else {
+    if (this.right){
+      return this.right.contains(value);
+    } else {
+      return false;
+    }
+  }
+return false;
 };
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-binarySearchTree.depthFirstLog = function(callback, n) {
-  var node;
-  if(arguments.length < 2){
-    node = this;
-  } else {
-  	node = n;
-  }
+binarySearchTree.depthFirstLog = function(callback,n) {
+var node = null;
+if (arguments.length < 2){
+  node = this;
+} else { 
+  node = n
+}
+callback(node.value)
+if (node.left){
+  this.depthFirstLog(callback,node.left)
+} 
+if (node.right) {
+  this.depthFirstLog(callback,node.right);
+}
 
-  callback(node.value);
-  if(node.left !== null){
-  	this.depthFirstLog(callback, node.left);
-  }
-  if(node.right !== null){
-  	this.depthFirstLog(callback, node.right);
-  }
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
